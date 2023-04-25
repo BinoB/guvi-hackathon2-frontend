7d591c1411abca0f416d9fe581246711c4e5627b
BinoB <binob831@gmail.com>
2023-04-25 10:28:38 +0530

    changr url

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  proxy : 'https://guvi-hackathon2.onrender.com',
       headers : {
         'Content-Type': 'application/json',
         authorization : `Bearer ${localStorage.getItem('token')}`
